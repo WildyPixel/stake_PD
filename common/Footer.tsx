35024fdfd7ee0968097d5b8ba8a207f3085f8516
WP <wildypixel@gmail.com>
2022-09-25 03:27:52 +0900

    Initial commit

---
+++ common/Footer.tsx
@@ -0,0 +1,116 @@
+import { darken, lighten } from 'polished'
+import { FaDiscord, FaTwitter } from 'react-icons/fa'
+
+import { contrastColorMode } from './utils'
+
+export const SOCIALS = {
+  discord: { icon: <FaDiscord />, link: 'https://discord.gg/xbDCUaJUgh' },
+  twitter: { icon: <FaTwitter />, link: 'https://twitter.com/PunkedDucks' },
+}
+
+export const Footer = ({
+  bgColor = 'rgb(26, 27, 32)',
+  accentColor = '#FFFFFF',
+}: {
+  bgColor?: string
+  accentColor?: string
+}) => {
+  return (
+    <div
+      className="mt-10 px-10 pt-5 md:px-32"
+      style={{
+        background: darken(0.03, bgColor),
+      }}
+    >
+      <div className="flex w-full flex-wrap items-start justify-between gap-10 py-10">
+        <div className="flex items-center">
+          <img
+            alt={bgColor}
+            className="inline-block h-[28px]"
+            src={'android-chrome-192x192.png'}
+          />
+          <span
+            className="ml-3 text-2xl font-semibold"
+            style={{ color: lighten(0.4, contrastColorMode(bgColor)[0]) }}
+          >
+            
+          </span>
+        </div>
+        <div className="flex gap-10 self-end text-center md:gap-20">
+          
+          <span className="flex flex-col items-start gap-1">
+            <div
+              className="mb-2 text-lg font-semibold"
+              style={{ color: lighten(0.4, contrastColorMode(bgColor)[0]) }}
+            >
+              Resources
+            </div>
+            <a href="https://duckzwitattitudes.com/whitepaper.html" className="text-gray-400">
+              Documentation
+            </a>
+            <a
+              href="https://solscan.io/token/3vaySGGghUAeVBxqFZkgvp9Fm33pcNcucrK9BhXUnK98"
+              className="text-gray-400"
+            >
+              Scan
+            </a>
+            <a href="mailto:duckzwitattitudes@gmail.com" className="text-gray-400">
+              Contact
+            </a>
+            {/*<a href="" className="text-gray-400">
+              Privacy
+            </a> */}
+          </span>
+          {/* <span className="flex flex-col items-start">
+            <div className="mb-5 text-lg font-semibold">Company</div>
+            <a href="https://www.cardinal.so/" className="text-gray-400">
+              Website
+            </a>
+            <a href="" className="text-gray-400">
+              Blog
+            </a>
+            <a
+              href="https://twitter.com/cardinal_labs"
+              className="text-gray-400"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://discord.com/invite/byq6uNTugq"
+              className="text-gray-400"
+            >
+              Discord
+            </a>
+          </span> */}
+        </div>
+      </div>
+      <div
+        className="text-md flex items-center justify-between border-t py-8 text-gray-400"
+        style={{ borderColor: lighten(0.2, bgColor) }}
+      >
+        <div className="flex items-center justify-center gap-2 text-gray-400">
+          Link
+        </div>
+        <div className="flex gap-4 text-gray-200">
+          {Object.entries(SOCIALS).map(([id, { icon, link }]) => {
+            return (
+              <a
+                key={id}
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                style={{ color: accentColor }}
+                className={`opacity-80 transition-opacity hover:text-primary hover:opacity-100`}
+              >
+                {icon}
+              </a>
+            )
+          })}
+        </div>
+      </div>
+      {/* <div className="text-md flex flex-row justify-center font-medium">
+        Copyright 2022 Cardinal Labs. All rights reserved
+      </div> */}
+    </div>
+  )
+}
---
+++ common/Header.tsx
@@ -0,0 +1,174 @@
+import { AccountConnect } from '@cardinal/namespaces-components'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { useWalletModal } from '@solana/wallet-adapter-react-ui'
+import { GlyphWallet } from 'assets/GlyphWallet'
+import { useStakePoolId } from 'hooks/useStakePoolId'
+import { useStakePoolMetadata } from 'hooks/useStakePoolMetadata'
+import { useRouter } from 'next/router'
+import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
+
+import { Airdrop } from './Airdrop'
+import { ButtonSmall } from './ButtonSmall'
+import { contrastColorMode } from './utils'
+import { asWallet } from './Wallets'
+
+export const Header = () => {
+  const router = useRouter()
+  const { environment, secondaryConnection } = useEnvironmentCtx()
+  const wallet = useWallet()
+  const walletModal = useWalletModal()
+  const stakePoolId = useStakePoolId()
+  const { data: stakePoolMetadata } = useStakePoolMetadata()
+
+  return (
+    <div>
+
+
+      <div
+        className={`mb-5 flex flex-wrap justify-between gap-6 px-5 pt-5 text-white`}
+        style={{ color: stakePoolMetadata?.colors?.fontColor }}
+      >
+
+        <a href={"https://punkedducks.com"}>
+          <img src={'/logo.png'}  style={{ height:"80px" }}></img>
+
+        </a>
+
+
+        <div className="flex items-center gap-3">
+
+        
+          <a
+            target="_blank"
+            href={
+              stakePoolMetadata?.websiteUrl ||
+              `/${
+                environment.label !== 'mainnet-beta'
+                  ? `?cluster=${environment.label}`
+                  : ''
+              }`
+            }
+            className="flex cursor-pointer text-xl font-semibold"
+            rel="noreferrer"
+          >
+            {stakePoolMetadata?.imageUrl ? (
+              <>
+                <div className="flex flex-row">
+                  <img
+                    className="flex h-[35px] flex-col rounded-lg"
+                    src={stakePoolMetadata?.imageUrl}
+                    alt={stakePoolMetadata?.imageUrl}
+                  />
+                  {stakePoolMetadata.nameInHeader && (
+                    <span
+                      className="ml-5 mt-1 flex flex-col"
+                      style={{ color: stakePoolMetadata?.colors?.fontColor }}
+                    >
+                      {stakePoolMetadata?.displayName}
+                    </span>
+                  )}
+                </div>
+                {stakePoolMetadata?.secondaryImageUrl && (
+                  <div className="ml-2 flex flex-row">
+                    <img
+                      className="flex h-[35px] flex-col"
+                      src={stakePoolMetadata?.secondaryImageUrl}
+                      alt={stakePoolMetadata?.secondaryImageUrl}
+                    />
+                    {stakePoolMetadata.nameInHeader && (
+                      <span
+                        className="ml-5 mt-1 flex flex-col"
+                        style={{ color: stakePoolMetadata?.colors?.fontColor }}
+                      >
+                        {stakePoolMetadata?.displayName}
+                      </span>
+                    )}
+                  </div>
+                )}
+              </>
+            ) : (
+              <div className="flex items-center justify-center gap-2 text-white">
+                {stakePoolMetadata?.displayName || (
+                  <img
+                    alt={'/cardinal-crosshair.svg'}
+                    className="inline-block w-4"
+                    src={'/cardinal-crosshair.svg'}
+                  />
+                )}{' '}
+                Staking
+              </div>
+            )}
+          </a>
+          {environment.label !== 'mainnet-beta' && (
+            <div className="cursor-pointer rounded-md bg-[#9945ff] p-1 text-[10px] italic text-white">
+              {environment.label}
+            </div>
+          )}
+          {environment.label !== 'mainnet-beta' ? (
+            <div className="mt-0.5">
+              <Airdrop />
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+        <div className="relative my-auto flex flex-wrap items-center gap-y-6 align-middle">
+          <div className="mr-10 flex flex-wrap items-center justify-center gap-8">
+            {stakePoolId && stakePoolMetadata ? (
+              stakePoolMetadata.links?.map((link) => (
+                <a
+                  key={link.value}
+                  href={link.value}
+                  className="cursor-pointer transition-all hover:opacity-80"
+                >
+                  <p style={{ color: stakePoolMetadata?.colors?.fontColor }}>
+                    {link.text}
+                  </p>
+                </a>
+              ))
+            ) : (
+              <>
+                <div
+                  onClick={() =>
+                    router.push(
+                      `/admin${
+                        environment.label !== 'mainnet-beta'
+                          ? `?cluster=${environment.label}`
+                          : ''
+                      }`
+                    )
+                  }
+                >
+                  <p className="my-auto mr-10 hover:cursor-pointer">Admin</p>
+                </div>
+              </>
+            )}
+          </div>
+          {wallet.connected && wallet.publicKey ? (
+            <AccountConnect
+              dark={
+                stakePoolMetadata?.colors?.backgroundSecondary
+                  ? contrastColorMode(stakePoolMetadata?.colors?.primary)[1]
+                  : true
+              }
+              connection={secondaryConnection}
+              environment={environment.label}
+              handleDisconnect={() => wallet.disconnect()}
+              wallet={asWallet(wallet)}
+            />
+          ) : (
+            <ButtonSmall
+              className="text-xs"
+              onClick={() => walletModal.setVisible(true)}
+            >
+              <>
+                <GlyphWallet />
+                <div className="text-white">Connect wallet</div>
+              </>
+            </ButtonSmall>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
